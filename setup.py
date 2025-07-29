f56d72f6937411681ed46076b0baf2cbda71640f
Shaun Kruger <skruger@uplynk.com>
2025-07-29 15:17:16 -0600

    Setup dynamic versioning and pyproject.toml

--- setup.py
+++ setup.py
@@ -1,31 +1,11 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
-import provider
 
 setup(
     name='django-oauth2',
-    version=provider.__version__,
-    description='Provide OAuth2 access to your app (fork of django-oauth2-provider)',
     long_description=open('README.rst').read(),
-    author='Shaun Kruger',
-    author_email='shaun.kruger@gmail.com',
-    url = 'https://github.com/stormsherpa/django-oauth2-provider',
     packages=find_packages(exclude=('tests*',)),
-    license='The MIT License: http://www.opensource.org/licenses/mit-license.php',
-    platforms='all',
-    classifiers=[
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Framework :: Django',
-    ],
-    install_requires=[
-        "shortuuid>=1.0.11",
-        "sqlparse>=0.4.3",
-    ],
     include_package_data=True,
     zip_safe=False,
 )
